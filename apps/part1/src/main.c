2801bb7106097fe1020ec07aa914d24ebe95f294
Graham Gobieski <graham.gobieski@gmail.com>
2018-10-05 17:40:18 -0400

    updated part 1 to not use pointers

--- apps/part1/src/main.c
+++ apps/part1/src/main.c
@@ -17,42 +17,35 @@
 #define COLS 513
 #define DCOLS 1
 
-__nv fixed buf[ROWS * DCOLS];
+__nv fixed result[ROWS * DCOLS];
 
-__nv mat_t mat_inputs = {
+__nv MAT_DECLARE(sample) = {
   .dims = {COLS, DCOLS},
 	.strides = {DCOLS, 1},
-  .len_dims = 2,
-  .data = sample
+  .len_dims = 2
 };
 
-__nv mat_t mat_weights = {
+__nv MAT_DECLARE(weights) = {
   .dims = {ROWS, COLS},
 	.strides = {COLS, 1},
   .len_dims = 2,
-  .data = weight
 };
 
-__nv mat_t mat_result = {
+__nv MAT_DECLARE(result) = {
   .dims = {ROWS, DCOLS},
 	.strides = {DCOLS, 1},
   .len_dims = 2,
-  .data = buf
 };
 
-__nv mat_t *input_ptr = &mat_inputs;
-__nv mat_t *weight_ptr = &mat_weights;
-__nv mat_t *result_ptr = &mat_result;
-
-void matmul(mat_t *result, mat_t *weights, mat_t *inputs) {
+void matmul() {
   uint16_t rows = MAT_GET_DIM(weights, 0);
   uint16_t cols = MAT_GET_DIM(weights, 1);
-  uint16_t dcols = MAT_GET_DIM(inputs, 1);
+  uint16_t dcols = MAT_GET_DIM(sample, 1);
   for(uint16_t i = 0; i < rows; i++) {
     for(uint16_t k = 0; k < dcols; k++) {
       fixed w = 0;
       for(uint16_t j = 0; j < cols; j++) {
-        fixed tmp = F_MUL(MAT_GET(inputs, j, k), MAT_GET(weights, i, j));
+        fixed tmp = F_MUL(MAT_GET(sample, j, k), MAT_GET(weights, i, j));
         w = F_ADD(w, tmp);
       }
       MAT_SET(result, w, i, k);
@@ -74,10 +67,13 @@ static void init_hw() {
 int main() {
     init_hw();
 		PRINTF("Starting...\r\n");
-    matmul(result_ptr, weight_ptr, input_ptr);
+    matmul();
 #ifdef CONSOLE
-    MAT_RESHAPE(result_ptr, 0, ROWS, DCOLS);
-    MAT_DUMP(result_ptr, 0);
+		for(uint16_t i = 0; i < ROWS; i++) {
+			for(uint16_t j = 0; j < DCOLS; j++) {
+				PRINTF("\r\n %i ", MAT_GET(result, i, j));
+			}
+		}
 #endif
     return 0;
 }
