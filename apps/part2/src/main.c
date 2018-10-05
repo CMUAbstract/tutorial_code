bd97a2fac0ff78c921ebbff8bad0eb78d46f5864
Graham Gobieski <graham.gobieski@gmail.com>
2018-10-05 17:47:28 -0400

    updated part 2 to not use pointers

--- apps/part2/src/main.c
+++ apps/part2/src/main.c
@@ -19,32 +19,25 @@
 #define COLS 513
 #define DCOLS 1
 
-__nv fixed buf[ROWS * DCOLS];
+__nv fixed result[ROWS * DCOLS];
 
-__nv mat_t mat_inputs = {
+__nv MAT_DECLARE(sample) = {
   .dims = {COLS, DCOLS},
 	.strides = {DCOLS, 1},
   .len_dims = 2,
-  .data = sample
 };
 
-__nv mat_t mat_weights = {
+__nv MAT_DECLARE(weights) = {
   .dims = {ROWS, COLS},
 	.strides = {COLS, 1},
   .len_dims = 2,
-  .data = weight
 };
 
-__nv mat_t mat_result = {
+__nv MAT_DECLARE(result) = {
   .dims = {ROWS, DCOLS},
 	.strides = {DCOLS, 1},
   .len_dims = 2,
-  .data = buf
 };
-
-__nv mat_t *input_ptr = &mat_inputs;
-__nv mat_t *weight_ptr = &mat_weights;
-__nv mat_t *result_ptr = &mat_result;
 
 void init();
 void task_init();
@@ -84,7 +77,7 @@ void task_init() {
 }
 
 void task_compute() {
-  uint16_t rows = MAT_GET_DIM(weight_ptr, 0);
+  uint16_t rows = MAT_GET_DIM(weights, 0);
 	if(GV(row_idx) < rows) {
 		TRANSITION_TO(task_dot_product);
 	}
@@ -92,21 +85,24 @@ void task_compute() {
 }
 
 void task_dot_product() {
-	uint16_t cols = MAT_GET_DIM(weight_ptr, 1);
+	uint16_t cols = MAT_GET_DIM(weights, 1);
 	fixed w = 0;
 	for(uint16_t i = 0; i < cols; i++) {
-		fixed tmp = F_MUL(MAT_GET(input_ptr, i, 0), MAT_GET(weight_ptr, GV(row_idx), i));
+		fixed tmp = F_MUL(MAT_GET(sample, i, 0), MAT_GET(weights, GV(row_idx), i));
 		w = F_ADD(w, tmp);
 	}
-	MAT_SET(result_ptr, w, GV(row_idx), 0);
+	MAT_SET(result, w, GV(row_idx), 0);
 	GV(row_idx)++;
 	TRANSITION_TO(task_compute);
 }
 
 void task_finish() {
 #ifdef CONSOLE
-    MAT_RESHAPE(result_ptr, 0, ROWS, DCOLS);
-    MAT_DUMP(result_ptr, 0);
+	for(uint16_t i = 0; i < ROWS; i++) {
+		for(uint16_t j = 0; j < DCOLS; j++) {
+			PRINTF("\r\n %i ", MAT_GET(result, i, j));
+		}
+	}
 #endif
 		exit(0);
 }
